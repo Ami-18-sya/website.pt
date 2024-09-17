b8175e607cbd7b0b69690559d25605dff803c0e6
Ami-18-sya <aminatuss18@gmail.com>
2024-09-17 08:53:55 -0700

    perubahan

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+include_once "./config/database.php";
+$name = "Ami Natus Syadiah";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+   <link rel="stylesheet" href="website.css">
+   <!--<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">-->
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+   <link rel="stylesheet" href="bootstrap-5.3.3-dist/css/bootstrap.css">
+
+   <title>Website</title>
+</head>
+<body>
+  
+</body>
