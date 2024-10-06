d00f1bd9f9992b389bab1658fb4eea0a2c278a83
Ami-18-sya <aminatuss18@gmail.com>
2024-10-06 09:35:24 -0700

    ubah contact.php,ubah documentation.php,ubah product.php

--- product.php
+++ product.php
@@ -1,10 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Project</title>
-</head>
+<?php
+include "./header.php"
+?>
 
 <body>
    <div class="container-fluid p-0">
