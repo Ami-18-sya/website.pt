390a2f5fc6be523c5e51d6a6342b9cb341f6f683
Ami-18-sya <aminatuss18@gmail.com>
2024-11-10 11:19:23 -0800

    ubah index.php,ubah home.php,ubah 1.svg,ubah 2.svg,ubah 3.svg

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ include __DIR__ . "/services/home.php";
 include "./header.php";
 
 $features = get_features($connection);
+$about = get_about($connection);
 
 ?>
 
@@ -52,40 +53,19 @@ $features = get_features($connection);
          echo $html;
       }
       ?>
-         <div class="item d-flex justify-content-center align-items-center flex-column" data-aos="fade-up" data-aos-delay="0" data-aos-duration="1000">
-            <svg xmlns="http://www.w3.org/2000/svg" width="75" height="75" fill="currentColor" class="bi bi-people-fill" viewBox="0 0 16 16">
-               <path d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6m-5.784 6A2.24 2.24 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.3 6.3 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1zM4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5" />
-            </svg>
-            <h6>Professional Team</h6>
-            <p>We have our professional team to manage our project</p>
-         </div>
-         <div class="item d-flex justify-content-center align-items-center flex-column" data-aos="fade-up" data-aos-delay="100" data-aos-duration="1000">
-            <svg xmlns="http://www.w3.org/2000/svg" width="75" height="75" fill="currentColor" class="bi bi-building-fill" viewBox="0 0 16 16">
-               <path d="M3 0a1 1 0 0 0-1 1v14a1 1 0 0 0 1 1h3v-3.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 .5.5V16h3a1 1 0 0 0 1-1V1a1 1 0 0 0-1-1zm1 2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm3 0a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm3.5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5M4 5.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zM7.5 5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5m2.5.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zM4.5 8h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5m2.5.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm3.5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5" />
-            </svg>
-            <h6>We deliver Quality</h6>
-            <p>Quality is our services priority</p>
-         </div>
-         <div class="item d-flex justify-content-center align-items-center flex-column" data-aos="fade-up" data-aos-delay="250" data-aos-duration="1000">
-            <svg xmlns="http://www.w3.org/2000/svg" width="75" height="75" fill="currentColor" class="bi bi-list-check" viewBox="0 0 16 16">
-               <path fill-rule="evenodd" d="M5 11.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5M3.854 2.146a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 1 1 .708-.708L2 3.293l1.146-1.147a.5.5 0 0 1 .708 0m0 4a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 1 1 .708-.708L2 7.293l1.146-1.147a.5.5 0 0 1 .708 0m0 4a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 0 1 .708-.708l.146.147 1.146-1.147a.5.5 0 0 1 .708 0" />
-            </svg>
-            <h6>Competitive</h6>
-            <p>We provide competitive price, schedule and method</p>
-         </div>
       </div>
    </div>
    <div id="about-section" class="py-4">
       <div class="container">
          <div id="about-title" class="d-flex justify-content-center mb-5">
-            <h5 class="fs-3 border-1 border-bottom border-black pb-3 px-4 fw-medium" data-aos="fade-up" data-aos-delay="0" data-aos-duration="1000">ABOUT US</h5>
+            <h5 class="fs-3 border-1 border-bottom border-black pb-3 px-4 fw-medium" data-aos="fade-up" data-aos-delay="0" data-aos-duration="1000"><?= $about['about_title'] ?></h5>
          </div>
          <div id="about-content" class="row px-5 mt-5 pt-5">
             <div id="text-panel" class="col-6">
-               <h5 class="fs-2 fw-medium text-center mb-3" data-aos="fade-up" data-aos-delay="0" data-aos-duration="1000">Who we are</h5>
-               <p style="text-align: justify;" data-aos="fade-up" data-aos-delay="0" data-aos-duration="1000">We are a construction company established in 2003. Our company able to manage and executed any project and to be your reliable partner in the Engineering, Procurement and Construction with committed to customer satisfaction oriented.</p>
+               <h5 class="fs-2 fw-medium text-center mb-3" data-aos="fade-up" data-aos-delay="0" data-aos-duration="1000"><?= $about['about_text_title'] ?></h5>
+               <p style="text-align: justify;" data-aos="fade-up" data-aos-delay="0" data-aos-duration="1000"><?= $about['about_text_paragraf_1'] ?></p>
 
-               <p style="text-align: justify;" data-aos="fade-up" data-aos-delay="0" data-aos-duration="1000">We have many experiences to manage any construction works for civil, structure, mechanical, piping, tank work and some electrical work at any industry such as :</p>
+               <p style="text-align: justify;" data-aos="fade-up" data-aos-delay="0" data-aos-duration="1000"><?= $about['about_text_paragraf_2'] ?></p>
 
                <ul data-aos="fade-up" data-aos-delay="0" data-aos-duration="1000">
                   <li>Power Plant</li>
--- services/home.php
+++ services/home.php
@@ -1,9 +1,49 @@
 <?php
 
 include_once __DIR__ . "/select.php";
+include_once __DIR__ . "/../sources/home.php";
+
+// section feature
+function feature_seeder($connection, $datas) : void {
+   $feature_count = mysqli_query($connection, "SELECT COUNT(*) AS count FROM home_feature");
+   if ($feature_count) {
+      $count = $feature_count->fetch_column();
+      if ($count <= 0) {
+         foreach ($datas as $key => $data) {
+            $title = $data['title'];
+            $description = $data['description'];
+            $icon_path = $data['icon_path'];
+            mysqli_query($connection, "INSERT INTO home_feature (id, title, description, icon_path) VALUES (NULL, '$title', '$description', '$icon_path')");
+         }
+      }
+   }
+}
+
+feature_seeder($connection, $feature_datas);
 
 function get_features($connection) {
-    return select($connection, "SELECT * FROM home_feature");
+   return select($connection, "SELECT * FROM home_feature");
+}
+// endsection
+
+function about_seeder($connection, $datas): void {
+   $db_count = mysqli_query($connection, "SELECT COUNT(*) AS count FROM home_about");
+   if ($db_count) {
+      $count = $db_count->fetch_column();
+      if ($count <= 0) {
+         $about_title = $datas['about_title'];
+         $about_text_title = $datas['about_text_title'];
+         $about_text_paragraf_1 = $datas['about_text_paragraf_1'];
+         $about_text_paragraf_2 = $datas['about_text_paragraf_2'];
+         mysqli_query($connection, "INSERT INTO home_about VALUES (NULL, '$about_title', '$about_text_title', '$about_text_paragraf_1', '$about_text_paragraf_2')");
+      }
+   }
+}
+
+about_seeder($connection, $abaout_datas);
+
+function get_about($connection) {
+   return select($connection, "SELECT * FROM home_about")->fetch_assoc();
 }
 
 ?>
