bc6eeca11943074ff71f8e8b390ca59fb2d73d9c
Ami-18-sya <aminatuss18@gmail.com>
2024-11-12 11:22:42 -0800

    ubah index.php,ubah home.php

--- index.php
+++ index.php
@@ -47,7 +47,7 @@ $about_items = get_about_items($connection);
       <?php
       foreach ($features as $key => $feature) {
          $html = "<div class='item d-flex justify-content-center align-items-center flex-column' data-aos='fade-up' data-aos-delay='0' data-aos-duration='1000'>";
-         $html .= "<img sec='...' alt='ICON'>";
+         $html .= "<img sec='/" . $feature['icon_path'] . "' alt='ICON'>";
          $html .= "<h6>" . $feature['title'] . "<h6>";
          $html .= "<p>" . $feature['description'] . "<p>";
          $html .="</div>";
