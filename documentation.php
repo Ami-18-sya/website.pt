02dde851579a7783d369204b084ddeb8b891a9fc
Ami-18-sya <aminatuss18@gmail.com>
2024-11-18 10:36:37 -0800

    ubah documentation.php,ubah product.php

--- documentation.php
+++ documentation.php
@@ -1,5 +1,8 @@
 <?php
-include "./header.php"
+
+include "./header.php";
+include __DIR__ . "/config/database.php";
+
 ?>
 
 <body>
@@ -12,16 +15,16 @@ include "./header.php"
       <div id="hero-section" class="w-100 d-flex justify-content-center align-items-center" style="height: 100vh;">
          <img src="./background.jpg" alt="" width="100%" style="height: 100%;" class="position-absolute">
          <div class="position-relative w-25 d-flex justify-content-center align-items-center flex-column">
-            <h2 class="border-bottom border-white text-center text-white fs-1 w-100 fw-semibold" style="text-shadow: 1px 1px 1px #1a1a1a;">Product Us</h2>
-            <a class="btn btn-primary" style="width: fit-content;" href="#product-section">MORE</a>
+            <h2 class="border-bottom border-white text-center text-white fs-1 w-100 fw-semibold" style="text-shadow: 1px 1px 1px #1a1a1a;">Produk Kami</h2>
+            <a class="btn btn-primary" style="width: fit-content;" href="#product-section">SELENGKAPNYA</a>
          </div>
       </div>
       <div id="product-section" class="py-4 mb-5">
          <div class="container-fluid">
             <div class="row justify-content-center my-5 py-5">
                <div class="col-12 col-lg-6">
-                  <h1 class="text-center" data-aos="fade-up" data-aos-delay="0" data-aos-duration="1000">Memberikan Produk Terbaik</h1>
-                  <p class="text-center fs-5" data-aos="fade-up" data-aos-delay="200" data-aos-duration="1000">Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt alias architecto est. Quis incidunt quibusdam, ullam vitae fugiat officia sapiente quas voluptatibus. Delectus fugit illum, autem a magni, error laudantium deserunt nam tempora vero alias. Dolores cum et optio quas.</p>
+                  <h1 class="text-center">Memberikan Produk Terbaik</h1>
+                  <p class="text-center fs-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt alias architecto est. Quis incidunt quibusdam, ullam vitae fugiat officia sapiente quas voluptatibus. Delectus fugit illum, autem a magni, error laudantium deserunt nam tempora vero alias. Dolores cum et optio quas.</p>
                </div>
             </div>
             <div class="row" id="product-list-wrapper">
@@ -49,7 +52,7 @@ include "./header.php"
          </div>
       </div>
    </div>
-   <?php
+   <?php 
    include "./footer.php"
    ?>
 </body>
