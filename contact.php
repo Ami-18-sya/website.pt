0bae1c952425d95867de563613b9dfb7e5606616
Ami-18-sya <aminatuss18@gmail.com>
2024-11-19 09:34:14 -0800

    ubah contact.php

--- contact.php
+++ contact.php
@@ -1,5 +1,10 @@
 <?php
-include "./header.php"
+include "./header.php";
+include __DIR__ . "/config/database.php";
+include __DIR__ . "/services/contact.php";
+
+$config  = get_contact_config($connection);
+
 ?>
 
 <body>
@@ -9,21 +14,21 @@ include "./header.php"
       include "./navbar.php"
 
       ?>
-      <div id="hero-section" class="w-100 d-flex justify-content-center" style="height: 100vh;">
+      <div id="hero-section" class="w-100 d-flex justify-content-center align-items-center" style="height: 100vh;">
          <img src="./background.jpg" alt="" width="100%" style="height: 100%;" class="position-absolute">
          <div class="position-relative w-25 d-flex justify-content-center align-items-center flex-column">
-         <h2 class="border-bottom border-white text-center text-white fs-1 w-100">CONTACT US</h2>
-         <a class="btn btn-primary" style="width: fit-content;" href="#contact-section">MORE</a>
-      </div>
+            <h2 class="border-bottom border-white text-center text-white fs-1 w-100"><?= $config['page_title'] ?></h2>
+            <a class="btn btn-primary text-uppercase" style="width: fit-content;" href="#contact-section"><?= $config['page_button_text'] ?></a>
+         </div>
       </div>
       <div id="contact-section">
          <div class="container py-5">
             <div class="row">
                <div class="col-12 mb-4">
-                  <h6 class="text-center fs-1 fw-bold" data-aos="fade-up" data-aos-delay="0" data-aos-duration="1000">CONTACT US</h6>
+                  <h6 class="text-center fs-1 fw-bold"><?= $config['section_title'] ?></h6>
                </div>
-               <div class="col-12 col-md-6 pt-4">
-                  <p class="fs-2 fw-semibold">Sampaikan pertanyaan, saran atau kritik melalui formulir berikut.</p>
+               <div class="col-12 col-md-6 pt-4 pe-0 pe-md-2">
+                  <p class="fs-2 fw-semibold"><?= $config['section_input_title'] ?></p>
                   <div class="col-12 d-flex flex-column gap-3">
                      <div class="form-group">
                         <label for="">Nama <small class="text-danger">*</small></label>
@@ -46,9 +51,9 @@ include "./header.php"
                      </div>
                   </div>
                </div>
-               <div class="col-12 col-md-6 pt-4">
+               <div class="col-12 col-md-6 pt-4 pe-0 pe-md-2">
                   <div class="mapouter">
-                     <div class="gmap_canvas"><iframe src="https://maps.google.com/maps?q=cilegon&amp;t=&amp;z=13&amp;ie=UTF8&amp;iwloc=&amp;output=embed" frameborder="0" scrolling="no" style="width: 590px; height: 400px;"></iframe>
+                     <div class="gmap_canvas"><iframe src="https://maps.google.com/maps?q=<?= $config['map_latitude'] ?>,<?= $config['map_longitude'] ?>&amp;t=&amp;z=13&amp;ie=UTF8&amp;iwloc=&amp;output=embed" frameborder="0" scrolling="no" style="width: 590px; height: 400px;"></iframe>
                         <style>
                            .mapouter {
                               display: table;
@@ -79,10 +84,10 @@ include "./header.php"
                      </div>
                   </div>
                   <div class="col-12 pt-4">
-                     <h2 class="fw-bold">COMPANY</h2>
+                     <h2 class="fw-bold text-uppercase"><?= $config['map_place_name'] ?></h2>
                   </div>
                   <div class="col-12">
-                     <p>Jl. Boulevard Blok A1 No.43 Komplek Taman Cilegon Indah, Kel.Sukmajaya, Kec.Jombang Kota Cilegon 42422</p>
+                     <p><?= $config['map_place_address'] ?></p>
                   </div>
                   <div class="col-12">
                      <p>example@example.com</p>
--- services/contact.php
+++ services/contact.php
@@ -1,14 +1,31 @@
 <?php
 
-$contact_config_data = [
-    "page_title" => "Kontak kami",
-    "page_button_text" => "Help",
-    "section_title" => "Kontak kami",
-    "section_input_title" => "Sampaikan pertanyaan, saran atau kritik melalui formulir berikut.",
-    "map_latitude" => "",
-    "map_longitude" => "",
-    "map_place_name" => "Kantor",
-    "map_place_address" => "Jl. Boulevard Blok A1 No.43 Komplek Taman Cilegon Indah, Kel.Sukmajaya, Kec.Jombang Kota Cilegon - BAANTEN kode pos 42422",
-];
+include_once __DIR__ . "/select.php";
+include_once __DIR__ . "/../sources/contact.php";
 
-?>
+function contact_seeder($connection, $config): void
+{
+   $db = mysqli_query($connection, "SELECT COUNT(*) FROM contact_config");
+   if ($db) {
+      $count = mysqli_fetch_assoc($db)["count"];
+      if ($count <= 0) {
+         $page_title = $config["page_title"];
+         $page_button_text = $config["page_button_text"];
+         $section_title = $config["section_title"];
+         $section_input_title = $config["section_input_title"];
+         $map_latitude = $config["map_latitude"];
+         $map_longitude = $config["map_longitude"];
+         $map_place_name = $config["map_place_name"];
+         $map_place_address = $config["map_place_address"];
+
+         mysqli_query($connection, "INSERT INTO contact_config VALUES (NULL, '$page_title', '$page_button_text', '$section_title', '$section_input_title', '$map_latitude', '$map_longitude', '$map_place_name', '$map_place_address')");
+      }
+   }
+}
+
+contact_seeder($connection, $contact_config_data);
+
+
+function get_contact_config($connection) {
+   return select($connection, "SELECT * FROM contact_config")->fetch_assoc();
+}
