5d5e22b16a69818b8b6db9e5b422184a5c4f819d
Ami-18-sya <aminatuss18@gmail.com>
2024-11-18 16:26:06 -0800

    ubah documentation.php

--- services/documentation.php
+++ services/documentation.php
@@ -1,60 +1,40 @@
 <?php
 
-include "./header.php";
-include __DIR__ . "/config/database.php";
+include_once __DIR__ . "/select.php";
+include_once __DIR__ . "/../sources/documentation.php";
 
-?>
+function documentation_seeder($connection, $config, $items): void
+{
+   $db = mysqli_query($connection, "SELECT COUNT(*) FROM documentation_config");
+   if ($db) {
+      $count = mysqli_fetch_assoc($db)["count"];
+      if ($count <= 0) {
+         $page_title = $config["page_title"];
+         $page_button_text = $config["page_button_text"];
+         $section_title = $config['section_title'];
+         $section_subtitle = $config['section_subtitle'];
+         mysqli_query($connection, "INSERT INTO documentation_config VALUES (NULL, '$page_title', '$page_button_text', '$section_title', '$section_subtitle')");
+      }
+   }
+   $db_item = mysqli_query($connection, "SELECT COUNT(*) FROM documentation_items");
+   if ($db_item) {
+      $count = mysqli_fetch_assoc($db_item)["count"];
+      if ($count <= 0) {
+         foreach ($items as $key => $item) {
+            $img = $item["img_path"];
+            $title = $item["title"];
+            $description = $item["description"];
+            mysqli_query($connection, "INSERT INTO documentation_items VALUES (NULL, '$img', '$title', '$description')");
+         }
+      }
+   }
+}
 
-<body>
-   <div class="container-fluid p-0">
-      <?php
+documentation_seeder($connection, $documentation_config_data, $documentation_item_datas);
 
-      include "./navbar.php"
-
-      ?>
-      <div id="hero-section" class="w-100 d-flex justify-content-center align-items-center" style="height: 100vh;">
-         <img src="./background.jpg" alt="" width="100%" style="height: 100%;" class="position-absolute">
-         <div class="position-relative w-25 d-flex justify-content-center align-items-center flex-column">
-            <h2 class="border-bottom border-white text-center text-white fs-1 w-100 fw-semibold" style="text-shadow: 1px 1px 1px #1a1a1a;">Produk Kami</h2>
-            <a class="btn btn-primary" style="width: fit-content;" href="#product-section">SELENGKAPNYA</a>
-         </div>
-      </div>
-      <div id="product-section" class="py-4 mb-5">
-         <div class="container-fluid">
-            <div class="row justify-content-center my-5 py-5">
-               <div class="col-12 col-lg-6">
-                  <h1 class="text-center">Memberikan Produk Terbaik</h1>
-                  <p class="text-center fs-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt alias architecto est. Quis incidunt quibusdam, ullam vitae fugiat officia sapiente quas voluptatibus. Delectus fugit illum, autem a magni, error laudantium deserunt nam tempora vero alias. Dolores cum et optio quas.</p>
-               </div>
-            </div>
-            <div class="row" id="product-list-wrapper">
-               <div class="col-12 col-md-6 col-lg-4 col-xl-3 p-0 overflow-hidden position-relative d-flex justify-content-center align-items-center" style="height: 300px;">
-                  <img src="./background.jpg" alt="..." width="100%" height="100%" class="position-absolute">
-                  <h2 class="text-center text-white position-relative">TEXT</h2>
-               </div>
-               <div class="col-12 col-md-6 col-lg-4 col-xl-3 p-0 overflow-hidden position-relative d-flex justify-content-center align-items-center" style="height: 300px;">
-                  <img src="./background.jpg" alt="..." width="100%" height="100%" class="position-absolute">
-                  <h2 class="text-center text-white position-relative">TEXT</h2>
-               </div>
-               <div class="col-12 col-md-6 col-lg-4 col-xl-3 p-0 overflow-hidden position-relative d-flex justify-content-center align-items-center" style="height: 300px;">
-                  <img src="./background.jpg" alt="..." width="100%" height="100%" class="position-absolute">
-                  <h2 class="text-center text-white position-relative">TEXT</h2>
-               </div>
-               <div class="col-12 col-md-6 col-lg-4 col-xl-3 p-0 overflow-hidden position-relative d-flex justify-content-center align-items-center" style="height: 300px;">
-                  <img src="./background.jpg" alt="..." width="100%" height="100%" class="position-absolute">
-                  <h2 class="text-center text-white position-relative">TEXT</h2>
-               </div>
-               <div class="col-12 col-md-6 col-lg-4 col-xl-3 p-0 overflow-hidden position-relative d-flex justify-content-center align-items-center" style="height: 300px;">
-                  <img src="./background.jpg" alt="..." width="100%" height="100%" class="position-absolute">
-                  <h2 class="text-center text-white position-relative">TEXT</h2>
-               </div>
-            </div>
-         </div>
-      </div>
-   </div>
-   <?php 
-   include "./footer.php"
-   ?>
-</body>
-
-</html>
+function get_documentation_config($connection){
+   return select($connection, "SELECT *FROM documentation_config")->fetch_assoc();
+}
+function get_documentation_items($connection){
+   return select($connection, "SELECT *FROM documentation_items");
+}
