cc8a535342ebeb2fe703a5e35f7b4d7bd3bc0d03
Ami-18-sya <aminatuss18@gmail.com>
2024-11-26 10:18:04 -0800

    ubah contact.php

--- sources/contact.php
+++ sources/contact.php
@@ -2,7 +2,7 @@
 
 $contact_config_data = [
    "page_title" => "Kontak Kami",
-   "page_button_text" => "help",
+   "page_button_text" => "Selengkapnya",
    "section_title" => "kontak kami",
    "section_input_title" => "Sampaikan pertanyaan, saran atau kritik melalui formulir berikut.",
    "map_latitude" => "-5.9684827",
